eb302465889626438f1882c7cff76080c91785be
nalkat <nalkat@yahoo.com>
2025-09-29 16:13:35 -0700

    Implement system-level simulation primitives

--- class_planet.php
+++ class_planet.php
@@ -1,6 +1,110 @@
 <?php // 7.3.0-dev
+
 class Planet extends SystemObject
 {
+        private $atmosphere;
+        private $habitable;
+        private $orbit;
 
+        public function __construct (string $name, float $mass = 0.0, float $radius = 0.0, ?array $position = null, ?array $velocity = null)
+        {
+                parent::__construct($name, $mass, $radius, $position, $velocity);
+                $this->setType('Planet');
+                $this->atmosphere = array();
+                $this->habitable = false;
+                $this->orbit = null;
+        }
+
+        public function setAtmosphere (array $composition) : void
+        {
+                $this->atmosphere = $composition;
+        }
+
+        public function getAtmosphere () : array
+        {
+                return $this->atmosphere;
+        }
+
+        public function setHabitable (bool $habitable) : void
+        {
+                $this->habitable = $habitable;
+        }
+
+        public function isHabitable () : bool
+        {
+                return $this->habitable;
+        }
+
+        public function getOrbit () : ?array
+        {
+                return $this->orbit;
+        }
+
+        public function setOrbit (SystemObject $focus, float $semiMajorAxis, float $period, float $eccentricity = 0.0, float $phase = 0.0) : bool
+        {
+                if ($semiMajorAxis <= 0)
+                {
+                        Utility::write("Semi-major axis must be positive", LOG_WARNING, L_CONSOLE);
+                        return false;
+                }
+                if ($period <= 0)
+                {
+                        Utility::write("Orbital period must be positive", LOG_WARNING, L_CONSOLE);
+                        return false;
+                }
+                if ($eccentricity < 0) $eccentricity = 0.0;
+                if ($eccentricity >= 1) $eccentricity = 0.999999;
+                $this->orbit = array(
+                        'focus' => $focus,
+                        'semi_major_axis' => floatval($semiMajorAxis),
+                        'period' => floatval($period),
+                        'eccentricity' => floatval($eccentricity),
+                        'angle' => floatval($phase)
+                );
+                $this->updateOrbit(0.0);
+                return true;
+        }
+
+        public function tick (float $deltaTime = 1.0) : void
+        {
+                if (($this->orbit !== null) && ($deltaTime > 0))
+                {
+                        $this->updateOrbit($deltaTime);
+                        $this->age += $deltaTime;
+                        return;
+                }
+                parent::tick($deltaTime);
+        }
+
+        private function updateOrbit (float $deltaTime) : void
+        {
+                if ($this->orbit === null) return;
+                $period = $this->orbit['period'];
+                if ($period <= 0) return;
+                $twoPi = 2 * pi();
+                if ($deltaTime > 0)
+                {
+                        $this->orbit['angle'] += $twoPi * ($deltaTime / $period);
+                }
+                $this->orbit['angle'] = fmod($this->orbit['angle'], $twoPi);
+                $focus = $this->orbit['focus'];
+                $focusPosition = $focus->getPosition();
+                $focusVelocity = $focus->getVelocity();
+                $ecc = $this->orbit['eccentricity'];
+                $semi = $this->orbit['semi_major_axis'];
+                $angle = $this->orbit['angle'];
+                $radius = $semi;
+                if ($ecc > 0)
+                {
+                        $radius = ($semi * (1 - ($ecc * $ecc))) / (1 + ($ecc * cos($angle)));
+                }
+                $this->position['x'] = $focusPosition['x'] + ($radius * cos($angle));
+                $this->position['y'] = $focusPosition['y'] + ($radius * sin($angle));
+                $this->position['z'] = $focusPosition['z'];
+                $angularSpeed = $twoPi / $period;
+                $this->velocity['x'] = $focusVelocity['x'] - ($radius * $angularSpeed * sin($angle));
+                $this->velocity['y'] = $focusVelocity['y'] + ($radius * $angularSpeed * cos($angle));
+                $this->velocity['z'] = $focusVelocity['z'];
+        }
 }
 ?>
--- class_system.php
+++ class_system.php
@@ -1,6 +1,174 @@
 <?php // 7.3.0-dev
-class System extends Galaxy
-{
 
+class System
+{
+        private $name;
+        private $objects;
+        private $primaryStar;
+        private $age;
+        private $timeStep;
+
+        public function __construct (string $name, ?Star $primaryStar = null)
+        {
+                $this->name = Utility::cleanse_string($name);
+                $this->objects = array();
+                $this->primaryStar = null;
+                $this->age = floatval(0);
+                $this->timeStep = floatval(60);
+                if ($primaryStar instanceof Star)
+                {
+                        $this->setPrimaryStar($primaryStar);
+                }
+        }
+
+        public function getName () : string
+        {
+                return $this->name;
+        }
+
+        public function getAge () : float
+        {
+                return $this->age;
+        }
+
+        public function setTimeStep (float $seconds) : void
+        {
+                if ($seconds <= 0)
+                {
+                        Utility::write("Time step must be greater than zero", LOG_WARNING, L_CONSOLE);
+                        return;
+                }
+                $this->timeStep = floatval($seconds);
+        }
+
+        public function getTimeStep () : float
+        {
+                return $this->timeStep;
+        }
+
+        public function setPrimaryStar (Star $star) : void
+        {
+                $this->primaryStar =& $star;
+                $this->registerObject($star);
+        }
+
+        public function getPrimaryStar () : ?Star
+        {
+                return $this->primaryStar;
+        }
+
+        public function addObject (SystemObject $object) : void
+        {
+                $this->registerObject($object);
+        }
+
+        public function addPlanet (Planet $planet, ?SystemObject $focus = null, ?float $semiMajorAxis = null, ?float $period = null, float $eccentricity = 0.0, float $phase = 0.0) : void
+        {
+                $this->registerObject($planet);
+                if (($focus instanceof SystemObject) && ($semiMajorAxis !== null) && ($period !== null))
+                {
+                        if ($planet->setOrbit($focus, $semiMajorAxis, $period, $eccentricity, $phase))
+                        {
+                                Utility::write(
+                                        $planet->getName() . " orbit registered around " . $focus->getName(),
+                                        LOG_INFO,
+                                        L_CONSOLE
+                                );
+                        }
+                }
+        }
+
+        private function registerObject (SystemObject $object) : void
+        {
+                $name = $object->getName();
+                if (isset($this->objects[$name]))
+                {
+                        Utility::write(
+                                "Replacing existing object $name in system " . $this->name,
+                                LOG_WARNING,
+                                L_CONSOLE
+                        );
+                }
+                $this->objects[$name] =& $object;
+                $object->attachToSystem($this);
+        }
+
+        public function removeObject (string $name) : ?SystemObject
+        {
+                if (!isset($this->objects[$name])) return null;
+                $object = $this->objects[$name];
+                unset($this->objects[$name]);
+                if ($object->getParentSystem() === $this)
+                {
+                        $object->attachToSystem(null);
+                }
+                if (($this->primaryStar !== null) && ($this->primaryStar->getName() === $name))
+                {
+                        $this->primaryStar = null;
+                }
+                return $object;
+        }
+
+        public function getObjects () : array
+        {
+                return $this->objects;
+        }
+
+        public function getObject (string $name) : ?SystemObject
+        {
+                if (!isset($this->objects[$name])) return null;
+                return $this->objects[$name];
+        }
+
+        public function getPlanets () : array
+        {
+                $planets = array();
+                foreach ($this->objects as $object)
+                {
+                        if ($object instanceof Planet)
+                        {
+                                $planets[$object->getName()] = $object;
+                        }
+                }
+                return $planets;
+        }
+
+        public function hasObject (string $name) : bool
+        {
+                return isset($this->objects[$name]);
+        }
+
+        public function countObjects () : int
+        {
+                return count($this->objects);
+        }
+
+        public function tick (?float $deltaTime = null) : void
+        {
+                $step = ($deltaTime === null) ? $this->timeStep : floatval($deltaTime);
+                if ($step <= 0) return;
+                foreach ($this->objects as $object)
+                {
+                        $object->tick($step);
+                }
+                $this->age += $step;
+        }
+
+        public function snapshotState () : array
+        {
+                $state = array();
+                foreach ($this->objects as $object)
+                {
+                        $state[$object->getName()] = array(
+                                'type' => $object->getType(),
+                                'mass' => $object->getMass(),
+                                'radius' => $object->getRadius(),
+                                'position' => $object->getPosition(),
+                                'velocity' => $object->getVelocity(),
+                                'age' => $object->getAge()
+                        );
+                }
+                return $state;
+        }
 }
 ?>
